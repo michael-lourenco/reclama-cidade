f4a492ee3d9d3e5a9f44be68aad7c1b80fec373c
Michael Lourenco <michael.lourenco@pixter.com.br>
2025-03-22 21:33:09 -0300

    remove search bar

--- src/components/map/map-fullscreen.tsx
+++ src/components/map/map-fullscreen.tsx
@@ -84,7 +84,7 @@ const MapFullScreen = () => {
                 />
             )}
 
-            {/* Top Bar - Search */}
+            {/* Top Bar */}
             <div className="absolute top-4 left-4 right-4 flex gap-2 z-10">
                 <Button
                     variant="default"
@@ -94,17 +94,6 @@ const MapFullScreen = () => {
                 >
                     <Menu className="h-5 w-5" />
                 </Button>
-                <div className="relative flex-1">
-                    <Input
-                        type="text"
-                        placeholder="Pesquisar localização..."
-                        className="w-full bg-white shadow-md pr-10"
-                        onKeyDown={(e) => e.key === "Enter" && console.log("search")}
-                    />
-                    <Button variant="ghost" size="icon" className="absolute right-0 top-0 h-full">
-                        <Search className="h-4 w-4" />
-                    </Button>
-                </div>
             </div>
             
     
